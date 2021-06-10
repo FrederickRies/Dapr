827f0801463b390aef24fe2992148c18e6bdcd8a
Fred 17268332+FrederickRies@users.noreply.github.com <17268332+FrederickRies@users.noreply.github.com>
2021-06-10 12:45:23 +0200

    Adding Directory.Build.props to force net6.0 and LangVersion to preview on all projects

--- ApiCatalog/Program.cs
+++ ApiCatalog/Program.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.Extensions.Hosting;
 using System;
+using System.Collections.Generic;
 
 var builder = WebApplication.CreateBuilder(args);
 await using var app = builder.Build();
@@ -10,6 +11,8 @@ if (app.Environment.IsDevelopment())
     app.UseDeveloperExceptionPage();
 }
 
-app.MapGet("/", (Func<string>)(() => "Hello World!"));
+app.MapGet("/", (Func<List<Item>>)(() => new List<Item>()));
 
 await app.RunAsync();
+
+public record Item(string Name, string Description, double Price);
--- ApiDeliveries/Program.cs
+++ ApiDeliveries/Program.cs
@@ -1,6 +1,9 @@
-using Microsoft.AspNetCore.Builder;
-using Microsoft.Extensions.Hosting;
-using System;
+global using Microsoft.AspNetCore.Builder;
+global using Microsoft.AspNetCore.Hosting;
+global using Microsoft.AspNetCore.Http;
+global using Microsoft.AspNetCore.Mvc;
+global using Microsoft.Extensions.Hosting;
+global using System;
 
 var builder = WebApplication.CreateBuilder(args);
 await using var app = builder.Build();
@@ -10,6 +13,16 @@ if (app.Environment.IsDevelopment())
     app.UseDeveloperExceptionPage();
 }
 
-app.MapGet("/", (Func<string>)(() => "Hello World!"));
+app.MapPost("/", (Func<string>)(() => "Hello World!"));
+app.MapPut("/{id}", async (Guid id) =>
+{
+    return new StatusCodeResult(StatusCodes.Status404NotFound);
+});
+app.MapDelete("/{id}", async (Guid id) =>
+{
+    return new StatusCodeResult(StatusCodes.Status404NotFound);
+});
 
 await app.RunAsync();
+
+public record Delivery(string Name, string Address, DateOnly Date);
