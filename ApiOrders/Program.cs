06910d5ea27e35ed199934efca6234342c2c4136
Fred 17268332+FrederickRies@users.noreply.github.com <17268332+FrederickRies@users.noreply.github.com>
2022-01-03 07:46:35 +0100

    Add API Gateway

--- ApiOrders/Program.cs
+++ ApiOrders/Program.cs
@@ -1,6 +1,9 @@
+using Dapr.Client;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.Extensions.Hosting;
 using System;
+using System.Net.Http;
+using System.Threading;
 
 var builder = WebApplication.CreateBuilder(args);
 await using var app = builder.Build();
@@ -10,6 +13,12 @@ if (app.Environment.IsDevelopment())
     app.UseDeveloperExceptionPage();
 }
 
-app.MapGet("/", (Func<string>)(() => "Hello World!"));
+app.MapGet("/items/{item-id}", async (string itemId, CancellationToken cancellationToken) =>
+{
+    using var client = new DaprClientBuilder().Build();
+    var result = client.CreateInvokeMethodRequest(HttpMethod.Get, "stocks", "items/" + itemId, cancellationToken);
+    await client.InvokeMethodAsync(result);
+
+});
 
 await app.RunAsync();
